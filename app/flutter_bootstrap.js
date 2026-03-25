89a0d3e8da7144500646d3fd521e212a40c07cbf
Alex2077 <alexander.abarca@gmx.com>
2026-03-25 18:25:44 -0300

    Deploy landing page + Flutter web app (2026-03-25 18:25:44)

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1774186561" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1135641748" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
